8e995b0f5ed7c23aa56443ee7b358ede5d3e9c56
kikipangestu <jekaymputh00@gmail.com>
2025-05-22 18:44:04 +0700

    update file

---
+++ db.sql
@@ -0,0 +1,8 @@
+CREATE TABLE hewan(
+    id INT AUTO_INCREMENT,
+    nama_hewan VARCHAR(255) NOT NULL,
+    jenis_hewan VARCHAR(255) NOT NULL,
+    jenis VARCHAR(50) NOT NULL,
+    usia SMALLINT(2) NOT NULL,
+    cover VARCHAR(255) NOT NULL,
+);
